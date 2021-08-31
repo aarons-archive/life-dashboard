8894583c74d2666e8d10eea585acafa939e3e6dc
Aaron Hennessey <axelancerr@gmail.com>
2021-08-31 04:27:47 +0100

    remove unused imports

--- Dashboard/typings/__init__.py
+++ Dashboard/typings/__init__.py
@@ -1,5 +1,2 @@
 # Future
 from __future__ import annotations
-
-# My stuff
-from typings.guild import GuildDict, GuildResponse, RelatedGuilds
--- Dashboard/utilities/objects/user.py
+++ Dashboard/utilities/objects/user.py
@@ -2,7 +2,6 @@
 from __future__ import annotations
 
 # Standard Library
-import json
 import time
 from typing import TYPE_CHECKING
 
